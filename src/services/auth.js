f2d45ef0e1b4dc893b93cd0ebe1dc637ac037b14
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2021-11-24 23:38:44 -0300

    Adicionar variavel para Token

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-export const TOKEN_KEY = "anex_token";
+export const TOKEN_KEY = process.env.REACT_APP_TOKEN_KEY || "anex_token";
 export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
